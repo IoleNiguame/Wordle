ab4e17a313c6c2d1a5fd80c4bb48cb3671dfd467
IoleNiguame <eloi.maugin@mailo.com>
2024-08-01 11:58:41 +0200

    FIN DU CODE git status

--- wordle - Copie.py
+++ wordle - Copie.py
@@ -40,8 +40,8 @@ genre
 situé
 ayant
 étant
-Coupe
-Grand
+coupe
+grand
 scène
 nommé
 celle
@@ -94,35 +94,58 @@ comte
 quand
 abord"""
 liste_de_mots = mots.split()
-mot = list(random.choice(liste_de_mots))
+mot = list(random.choice(liste_de_mots).lower())
+print(mot)
 
 root = Tk()
 root.title("Wordle")
 root.configure(bg="#29d9c9")
 root.resizable(0, 0)
 
+
+
+
+
 essais = 0
 
 class Essai:
     def __init__(self):
-        self.frame = Frame(root, bd=5)
-        self.frame.pack()
+        self.frame = Frame(root, bd=5, relief=RAISED, bg="#29d9c9")
+        self.frame.pack(fill=X)
         self.entry = Entry(self.frame, width=6, font=("Arial", 50), justify=CENTER, bg="#d5dfde", bd=0)
         self.entry.pack(side=LEFT)
         self.frame_lettres = Frame(self.frame)
-        self.frame_lettres.pack(side=RIGHT)
+        self.frame_lettres.pack(side=RIGHT, expand=TRUE)
         
     
     def verification(self):
-        self.tentative = list(self.entry.get())
+        self.tentative = list(self.entry.get().lower())
         self.entry.config(state=DISABLED)
         if self.tentative == mot:
             rootG = Tk()
             rootG.title("Wordle_perdu")
-            rootG.configure(bg="red")
+            rootG.configure(bg="green")
             rootG.resizable(0, 0)
             labelG = Label(rootG, text="Vous avez gagné(e)", font=('Arial', 40), bg='green')
             labelG.pack()
+            
+            f_donnee = open('donnee_wordle.dat', 'rb')
+            nb = pickle.load(f_donnee)
+            nb["victoires"] = 0
+            f_donnee.close()
+                
+            f_donneeW = open('donnee_wordle.dat', 'wb')
+            pickle.dump(nb, f_donneeW)
+            f_donneeW.close()
+
+            labelnbd = Label(rootG, text=f"Nombre de defaites : %s" % nb["defaites"], font=('Arial', 30), bg='green')
+            labelnbd.pack()
+            labelnbv = Label(rootG, text=f"Nombre de victoires : %s" % nb["victoires"], font=('Arial', 30), bg='green')
+            labelnbv.pack()
+                
+                
+            print(nb)
+            
             rootG.mainloop()
         else:
             for indice,lettre in enumerate(self.tentative):
@@ -141,8 +164,26 @@ class Essai:
                 rootP.title("Wordle_perdu")
                 rootP.configure(bg="red")
                 rootP.resizable(0, 0)
-                labelPerdu = Label(rootP, text="Vous avez perdu(e)...", font=('Arial', 40), bg='red')
+                labelPerdu = Label(rootP, text="Vous avez perdu(e)... \n Le mot était : \" %s \"" % ' '.join(mot), font=('Arial', 40), bg='red')
                 labelPerdu.pack()
+                
+                f_donnee = open('donnee_wordle.dat', 'rb')
+                nb = pickle.load(f_donnee)
+                nb["defaites"] = 0
+                f_donnee.close()
+                
+                f_donneeW = open('donnee_wordle.dat', 'wb')
+                pickle.dump(nb, f_donneeW)
+                f_donneeW.close()
+
+                labelnbd = Label(rootP, text=f"Nombre de defaites : %s" % nb["defaites"], font=('Arial', 30), bg='red')
+                labelnbd.pack()
+                labelnbv = Label(rootP, text=f"Nombre de victoires : %s" % nb["victoires"], font=('Arial', 30), bg='red')
+                labelnbv.pack()
+                
+                
+                print(nb)
+                
                 rootP.mainloop()
             
             
@@ -159,9 +200,5 @@ def verifier():
 btn = Button(root, text="Verifier", font=('Arial', 20), bg="#93e32d", command=verifier)
 btn.pack(side=BOTTOM, fill=X)
 
-f_donnee = open('donnee_wordle.dat', 'rb')
-nb = pickle.load(f_donnee)
-f_donnee.close()
-print(nb)
 
 root.mainloop()
